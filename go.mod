4298250944b5b67bb4ae2f5d58fba766bb109299
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2024-03-05 02:16:42 +0530

    made simple proxy in go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Mohd-Sayeedul-Hoda/go_proxy
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+var defaultConfig = http.DefaultTransport
+
+func main(){
+  http.HandleFunc("/", handleRequest)
+
+  log.Println("server running on port 8000")
+  http.ListenAndServe(":8000", nil)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request){
+  targetURL := r.URL
+  proxyRequest, err := http.NewRequest(r.Method, targetURL.String(), r.Body)
+  if err != nil{
+    http.Error(w, "you request can't be resolve", http.StatusInternalServerError)
+    return
+  }
+
+  for name, values := range r.Header{
+    for _, value := range values{
+      r.Header.Set(name, value)
+    }
+  }
+
+  resp, err := defaultConfig.RoundTrip(proxyRequest)
+  if err != nil{
+    http.Error(w, "error while sending request", http.StatusInternalServerError)
+  }
+  defer resp.Body.Close()
+
+  for name, values := range r.Header{
+    for _, value := range values{
+      w.Header().Add(name, value)
+    }
+  }
+
+  io.Copy(w, resp.Body)
+}
